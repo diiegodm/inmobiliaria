b6c9f4085f2e8bb71167f056c3caf6211874dc17
diiegodm <diegoarevalodm@gmail.com>
2025-03-03 14:32:24 +0100

    correccion del intersector

--- inmobiliaria/front_bienes_raices/bienes_raices/src/app/auth/interceptors/token.interceptor.ts
+++ inmobiliaria/front_bienes_raices/bienes_raices/src/app/auth/interceptors/token.interceptor.ts
@@ -1,18 +1,31 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
-  const role = localStorage.getItem('role')
-  
+  const role = localStorage.getItem('role');
+  const router = inject(Router);
+
   if (token && role) {
-  
-  req = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`,
-      Role: role
-    }
-  });
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+        Role: role
+      }
+    });
   }
-  
-    return next(req);
-  };
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) { 
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+        router.navigate(['/login']); // Redirigir al login si el token es inválido
+      }
+      return throwError(() => error);
+    })
+  );
+};
